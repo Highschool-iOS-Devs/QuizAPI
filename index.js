ca6117db0834f9c3d1d3d29c27aa4e5b58adfb4c
Andreas Ink <andreasink17@gmail.com>
2021-07-26 00:44:19 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const multer = require('multer')
+const fs = require('fs')
+const { promisify } = require('util')
+
+const unlinkAsync = promisify(fs.unlink)
+const app = express()
+const port = process.env.PORT || 3001
+//const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, callback) {
+      callback(null, file.originalname)
+  }
+});
+var text = "...";
+var quiz = "...";
+
+app.post('/postText', function(req, res) {
+    text = req.query.text
+    res.send(text)
+    return 'hello'
+})
+app.get('/getText', function(req, res) {
+    res.send(text)
+    return 'hello'
+})
+app.get('/postQuiz', function(req, res) {
+    quiz = req.query.quiz
+    text = ""
+    res.send(quiz)
+    return 'hello'
+})
+app.get('/getQuiz', function(req, res) {
+    res.send(quiz)
+    return 'hello'
+})
+app.listen(port)
+console.log('Server started successfully on port ' + port + "!")
