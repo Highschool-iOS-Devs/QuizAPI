18352a3e7aaa376599d50b34fbae0fd528789ae4
Andreas Ink <andreasink17@gmail.com>
2021-07-26 02:48:07 -0400

    a

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/getText', function(req, res) {
     return 'hello'
 })
 app.post('/postQuiz', function(req, res) {
-    quiz = req.query.quiz
+    quiz = req.query
     text = "..."
     res.send({"quiz": quiz})
     return 'hello'
