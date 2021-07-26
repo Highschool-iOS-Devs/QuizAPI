e48e631a939635b25569dd6010ee627f1c4c2567
Andreas Ink <andreasink17@gmail.com>
2021-07-26 15:59:07 -0400

    a

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.get('/postQuiz', function(req, res) {
     return 'hello'
 })
 app.get('/getQuiz', function(req, res) {
-    res.sendFile(__dirname + "/uploads/" + "quiz.json");
-    
+    //res.sendFile(__dirname + "/uploads/" + "quiz.json");
+    res.send({"quiz": quiz})
     return 'hello'
 })
 app.post('/upload', upload.single('uploadedFile'), (req, res) => {
